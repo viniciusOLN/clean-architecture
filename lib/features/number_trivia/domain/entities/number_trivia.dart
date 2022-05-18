bfbabfd6893cb19499a0fd9c0a8ac2c1e37faee2
vinicius <vinicius853o@gmail.com>
2022-05-18 19:22:38 -0300

    adding the entities of the feature and the repositories(contract)

---
+++ lib/features/number_trivia/domain/entities/number_trivia.dart
@@ -0,0 +1,15 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+class NumberTrivia extends Equatable {
+  final String text;
+  final int number;
+
+  const NumberTrivia({
+    @required required this.text,
+    @required required this.number,
+  }) : super();
+
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
