f8781849ec111e9d5174a79620af8603e0b4d049
vinicius <vinicius853o@gmail.com>
2022-05-18 21:53:09 -0300

    update

--- lib/features/number_trivia/domain/repositories/number_trivia.dart
+++ lib/features/number_trivia/domain/repositories/number_trivia.dart
@@ -2,7 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:trivia_app/core/error/failures.dart';
 import 'package:trivia_app/features/number_trivia/domain/entities/number_trivia.dart';
 
-abstract class NumberTriviaRepositorie {
+abstract class NumberTriviaRepository {
   Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
   Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
 }
