9a6634d134f92f6da88a94797e87bf1b9eb648fe
vinicius <vinicius853o@gmail.com>
2022-05-18 21:54:32 -0300

    adding test to usecase get concrete trivia number

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:trivia_app/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:trivia_app/features/number_trivia/domain/repositories/number_trivia.dart';
+
+import '../../../../core/error/failures.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+
+  GetConcreteNumberTrivia(this.repository);
+
+  Future<Either<Failure, NumberTrivia>> execute({required int number}) async {
+    return await repository.getConcreteNumberTrivia(number);
+  }
+}
