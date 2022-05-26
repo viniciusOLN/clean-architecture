d93822660f76ae3f7d7541a50eddc3bc0d7095cd
vinicius <vinicius853o@gmail.com>
2022-05-25 22:03:45 -0300

    refactoring the usecase getConcreteNumberTrivia

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,15 +1,17 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:trivia_app/core/usecases/usecases.dart';
 import 'package:trivia_app/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:trivia_app/features/number_trivia/domain/repositories/number_trivia.dart';
-
 import '../../../../core/error/failures.dart';
 
-class GetConcreteNumberTrivia {
+class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Params> {
   final NumberTriviaRepository repository;
 
   GetConcreteNumberTrivia(this.repository);
 
-  Future<Either<Failure, NumberTrivia>> execute({required int number}) async {
-    return await repository.getConcreteNumberTrivia(number);
+  @override
+  Future<Either<Failure, NumberTrivia>> call(Params params) async {
+    return await repository.getConcreteNumberTrivia(params.number);
   }
 }
