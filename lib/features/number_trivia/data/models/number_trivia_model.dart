6eea311417c19ebeb0ddc18e3eaa2ad784d46170
vinicius <vinicius853o@gmail.com>
2022-05-27 16:44:29 -0300

    creating new metods to the class

--- lib/features/number_trivia/data/models/number_trivia_model.dart
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -5,4 +5,18 @@ class NumberTriviaModel extends NumberTrivia {
     required int number,
     required String text,
   }) : super(text: text, number: number);
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+      number: (json['number'] as num).toInt(),
+      text: json['text'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': text,
+      'number': number,
+    };
+  }
 }
