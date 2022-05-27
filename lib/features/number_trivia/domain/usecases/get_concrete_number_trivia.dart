d09e1a7dc1dd70ab67d129c0c3711e65c254d0bd
vinicius <vinicius853o@gmail.com>
2022-05-27 16:45:11 -0300

    updating usecase

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,5 +1,4 @@
 import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
 import 'package:trivia_app/core/usecases/usecases.dart';
 import 'package:trivia_app/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:trivia_app/features/number_trivia/domain/repositories/number_trivia.dart';
