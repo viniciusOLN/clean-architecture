7698c809a0e7d06f0026db0ef167359babf6b550
vinicius <vinicius853o@gmail.com>
2022-05-27 23:31:08 -0300

    adding class to handle failures on the server side or cache side

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -3,3 +3,19 @@ import 'package:equatable/equatable.dart';
 abstract class Failure extends Equatable {
   const Failure([List properties = const <dynamic>[]]) : super();
 }
+
+class ServerFailure extends Failure {
+  final String message;
+  const ServerFailure(this.message);
+
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
+
+class CacheFailure extends Failure {
+  final String message;
+  const CacheFailure(this.message);
+
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
