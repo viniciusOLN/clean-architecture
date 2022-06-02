6bb8b8040187fe778066d94e23e61265eaaf9ea5
vinicius <vinicius853o@gmail.com>
2022-06-01 21:45:47 -0300

    adding implementation of the data

--- lib/features/number_trivia/data/repositories/number_trivia_repository_imp.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_imp.dart
@@ -1,3 +1,4 @@
+import 'package:trivia_app/core/error/exceptions.dart';
 import 'package:trivia_app/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:trivia_app/core/error/failures.dart';
 import 'package:dartz/dartz.dart';
@@ -19,12 +20,31 @@ class NumberTriviaRepositoryImplementation implements NumberTriviaRepository {
   });
 
   @override
-  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) {
-    return null;
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(
+      int number) async {
+    if (await networkInfo.isConnected) {
+      networkInfo.isConnected;
+      try {
+        final remoteTrivia =
+            await remoteDataSource.getConcreteNumberTrivia(number);
+        localDataSource.cacheNumberTrivia(remoteTrivia);
+        return Right(remoteTrivia);
+      } on ServerException {
+        return const Left(ServerFailure());
+      }
+    } else {
+      try {
+        final localTrivia = await localDataSource.getLastNumberTrivia();
+        return Right(localTrivia);
+      } on CacheException {
+        return const Left(CacheFailure());
+      }
+    }
   }
 
   @override
-  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() {
-    return null;
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() async {
+    NumberTrivia a = const NumberTrivia(text: '', number: 2);
+    return Right(a);
   }
 }
